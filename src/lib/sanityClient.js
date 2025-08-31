e411b56295126ccd95ed5138ed5a3fa1e0fa4ca2
Moe <72208447+mhamidawad@users.noreply.github.com>
2025-08-31 03:07:21 +0300

    Update sanityClient.js

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -6,7 +6,7 @@ console.log("Sanity Project ID from ENV:", import.meta.env.PUBLIC_SANITY_PROJECT
 
 export const client = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: "production",
+  dataset: "last",
   useCdn: false, // Set to true if you are using static data for the frontend
   apiVersion: "2025-08-31", // use current date (YYYY-MM-DD) to prevent errors
 });
