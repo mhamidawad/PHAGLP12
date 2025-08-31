fcf2ddad2e3446b0b721e14e21cf1244551a93d9
Moe <72208447+mhamidawad@users.noreply.github.com>
2025-08-31 03:34:01 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
-import sanityIntegration from "@sanity/astro";
-import react from "@astrojs/react"; // Import the React integration
+import { sanityIntegration } from "@sanity/astro"; // Changed to a named import
+import react from "@astrojs/react";
 import { loadEnv } from "vite";
 
 const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
@@ -16,10 +16,8 @@ export default defineConfig({
       projectId: PUBLIC_SANITY_PROJECT_ID,
       dataset: PUBLIC_SANITY_DATASET,
       useCdn: true,
-      // Define the base path where the Sanity Studio will be available
-      // For example, this will make the studio accessible at yoursite.com/admin
       studioBasePath: "/admin",
     }),
-    react(), // Add the React integration to your project
+    react(),
   ],
 });
